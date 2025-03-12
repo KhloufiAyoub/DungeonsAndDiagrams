8469df0a3f0225a3f379856b3fde69ee39ed445e
Ayoub <ayoub.khloufi@condorcet.be>
2025-03-12 13:46:20 +0100

    Login

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 import psycopg
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
-
+def login():
+    return render_template("login.html")
 
 if __name__ == '__main__':
     app.run()
