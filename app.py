b98fbb4da8d2d3f593813ec57bd19b93c8ed0395
Ayoub <ayoub.khloufi@condorcet.be>
2025-03-31 23:52:26 +0200

    a

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import psycopg, re,hashlib
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, session
 
 app = Flask(__name__)
+app.secret_key = "cruipi72failou"
 
 conn = psycopg.connect(
     "dbname=py2501 user=py2501 password=cruipi72failou host=student.endor.be port=5433"
@@ -17,6 +18,8 @@ def home():
 
 @app.route('/game', methods=['POST'])
 def submit():
+    session["level_id"] = None
+    session["user_id"] = None
     try:
         if request.method == 'POST':
             username = request.form['username']
@@ -35,22 +38,22 @@ def submit():
                 with conn.cursor() as cursor:
                     cursor.execute("SELECT * FROM usr WHERE login = %s", (username,))
                     existing_user = cursor.fetchone()
-
+                    session["level_id"] = 15
                     if existing_user:
                         # Vérifier si le mot de passe correspond
                         if existing_user[2] == hashlib.sha256(password.encode()).hexdigest():
                             # Rediriger vers une page de succès ou une autre page appropriée
+                            session["user_id"] = existing_user[0]
                             return render_template('game.html')
                         else:
+                            session["level_id"] = None
                             return render_template('login.html', error="Le mot de passe est incorrect.")
                     else:
                         # Insérer les données dans la base de données
-                        cursor.execute("INSERT INTO usr (login,passwd) VALUES (%s, %s)",(username, hashlib.sha256(password.encode()).hexdigest()))
+                        cursor.execute("INSERT INTO usr (login,passwd) VALUES (%s, %s) RETURNING uid",(username, hashlib.sha256(password.encode()).hexdigest()))
+                        session["user_id"] = cursor.fetchone()[0]
                         conn.commit()
-
-                        cursor.execute("SELECT lvl,hinth,hintv FROM levels WHERE lid = 1")
-                        level = cursor.fetchone()
-                        return render_template('game.html', level=level[0], hinth=level[1], hintv=level[2])
+                        return render_template('game.html')
             else:
                 return render_template('login.html', error="Nom d'utilisateur ou mot de passe incorrect.")
 
@@ -61,14 +64,14 @@ def submit():
 def init():
     try:
         with conn.cursor() as cursor:
-            cursor.execute("SELECT lvl, hinth, hintv FROM levels WHERE lid = 1")
+            cursor.execute("SELECT lvl, hinth, hintv,lid, name FROM levels WHERE lid = %s", (session["level_id"],))
             level = cursor.fetchone()
             if level:
                 return jsonify(level)
             else:
-                return jsonify({"error": "No level found"}), 404
+                return jsonify({"message": "No level found"})
     except Exception as e:
-        return jsonify({"error": str(e)}), 500
+        return jsonify({"message": str(e)})
 
 if __name__ == '__main__':
     app.run(debug=True)
--- static/game.js
+++ static/game.js
@@ -24,45 +24,70 @@ function loadGame(){
 }
 
 function initializeGrid(str) {
-    var i
-    hinth = str[1]
-    hintv = str[2]
-    for ( i=0; i < str[0].length; i++) {
-        const cell = document.getElementById(`${Math.floor(i / 8)};${i % 8}`);
-        if (str[0][i] === "3") {
-            cell.classList.add('chest');
-            grid[Math.floor(i / 8)][i % 8] = 3;
-        }
-        if (str[0][i] === "2") {
-            cell.classList.add('monster');
-            grid[Math.floor(i / 8)][i % 8] = 2;
-        }
-        (function(i) {
-            cell.addEventListener('mousedown', function(event) {
-                if (event.button === 0) { // Left-click
-                    if (cell.classList.contains('wall')) {
-                        cell.classList.remove('wall');
+    if(str["message"] !== "No level found"){
+        document.body.style.display = 'block';
+        document.getElementById("titre").innerHTML = "Niveau " + str[3] + " : " + str[4]
+        var i
+        hinth = str[1]
+        hintv = str[2]
+        for ( i=0; i < str[0].length; i++) {
+            const cell = document.getElementById(`${Math.floor(i / 8)};${i % 8}`);
+            if (str[0][i] === "3") {
+                cell.classList.add('chest');
+                grid[Math.floor(i / 8)][i % 8] = 3;
+            }
+            if (str[0][i] === "2") {
+                cell.classList.add('monster');
+                grid[Math.floor(i / 8)][i % 8] = 2;
+            }
+            (function(i) {
+                cell.addEventListener('mousedown', function(event) {
+                    if (event.button === 0) { // Left-click
+                        if(grid[Math.floor(i / 8)][i % 8] !== 3 && grid[Math.floor(i / 8)][i % 8] !== 2){
+                            if (cell.classList.contains('wall')) {
+                                cell.classList.remove('wall');
+                                grid[Math.floor(i / 8)][i % 8] = 0;
+                            } else {
+                                cell.classList.add('wall');
+                                cell.classList.remove('floor');
+                                grid[Math.floor(i / 8)][i % 8] = 1;
+                            }
+                        }
+                    } else if (event.button === 2) { // Right-click
+                        if(grid[Math.floor(i / 8)][i % 8] !== 3 && grid[Math.floor(i / 8)][i % 8] !== 2){
+                            if (cell.classList.contains('floor')) {
+                                cell.classList.remove('floor');
+                            } else {
+                                cell.classList.add('floor');
+                                cell.classList.remove('wall');
+                            }
+                        }
                         grid[Math.floor(i / 8)][i % 8] = 0;
-                    } else {
-                        cell.classList.add('wall');
-                        cell.classList.remove('floor');
-                        grid[Math.floor(i / 8)][i % 8] = 1;
                     }
-                } else if (event.button === 2) { // Right-click
-                    cell.classList.add('floor');
-                    cell.classList.remove('wall');
-                    grid[Math.floor(i / 8)][i % 8] = 0;
-                }
-                CheckNumbers()
+                    var id = cell.id.split(";")
+                    UpdateNumbers(id[0], id[1])
+                    CheckNumbers()
+                });
+            })(i);
+            // Prevent context menu on right-click
+            cell.addEventListener('contextmenu', function(event) {
+                event.preventDefault();
             });
-        })(i);
-        // Prevent context menu on right-click
-        cell.addEventListener('contextmenu', function(event) {
-            event.preventDefault();
-        });
-    }
-}
 
+            for(var j =0; j<8;j++){
+                document.getElementById("row-"+j).innerHTML = hintv[j]
+                document.getElementById("col-"+j).innerHTML = hinth[j]
+            }
+
+            for(j =0; j<8;j++){
+                UpdateNumbers(j,j)
+            }
+        }
+    }else{
+        window.location.href = "/lost";
+    }
+
+}
 function CheckNumbers() {
     for (let i = 0; i < grid.length; i++) {
         let cpth=0
@@ -74,19 +99,33 @@ function CheckNumbers() {
             if(grid[j][i] === 1){
                 cpth++;
             }
-
         }
-        if(cptv !== parseInt(hintv[i])){
-            console.log("ligne " + i + " : " +cptv, hintv[i])
-            return
-        }else if(cpth !== parseInt(hinth[i])){
-            console.log("colonne " + i + " : "+ cpth, hinth[i])
+        if(cptv !== parseInt(hintv[i]) || cpth !== parseInt(hinth[i])){
+            document.getElementById("nextLevelBtn").style.display = "none";
             return
         }
     }
-
-    console.log("ok")
-
+    document.getElementById("nextLevelBtn").style.display = "block";
 }
-
-loadGame()
+function UpdateNumbers(row, col){
+    var cptRow =0
+    var cptCol =0
+    for(var i =0; i<8;i++){
+        if(grid[row][i] === 1){
+            cptRow++;
+        }
+        if (grid[i][col] === 1){
+            cptCol++;
+        }
+    }
+    if(cptRow === parseInt(hintv[row])) {
+        document.getElementById("row-"+row).style.color = "green";
+    }else{
+        document.getElementById("row-"+row).style.color = "red";
+    }
+    if(cptCol === parseInt(hinth[col])) {
+        document.getElementById("col-"+col).style.color = "green";
+    }else {
+        document.getElementById("col-" + col).style.color = "red";
+    }
+}
